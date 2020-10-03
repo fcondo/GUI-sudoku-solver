8a8ac45bf0143b8705f898a23ee0ba50ee99c9af
fcondo <fccondo7@gmail.com>
2020-10-03 14:32:59 +0200

    Func() for new game and selection movement through keys

--- sudoku_GUI.py
+++ sudoku_GUI.py
@@ -5,6 +5,9 @@
 """
 import time
 import pygame
+
+from copy import deepcopy
+from solver import solve
 from math import floor
 
 
@@ -30,41 +33,39 @@ class Cube:
         self.col = col
         self.val = val
         self.temp = 0
-        self.fixed = False
+        self.is_grid = False
         self.width = width
         self.height = height
         self.selected = False
 
     def draw(self, win):
-        font = pygame.font.SysFont('Comic Sans MS', 30)
-        
+        size = 60
         x = self.row * self.width
         y = self.col * self.height
+        s = ''
+        color = (125, 125, 125)
+        pos_x = x + self.width/2 - 12
+        pos_y = y + self.height/2 - 15
 
-        if(self.fixed):
+        if(self.is_grid):
+            color = (125, 125, 125)
             s = self.val
-            pos_x = self.width/2 - 30
-            pos_y = self.height/2 - 30
-
-            if(self.val != 0):
-                s = self.val
-            elif(self.temp != 0):
-                s = self.temp
-                pos_x = x + 5
-                pos_y = y + 5
-            else:
-                s = ''
-                pos_x = x + 5
-                pos_y = y + 5
-        else:
-            s = ''#self.val
-            pos_x = self.width/2 - 30
-            pos_y = self.height/2 - 30
+        elif(self.val != 0):
+            color = (0, 0, 0)
+            s = self.val
+            
+        # elif(not self.is_grid and self.temp != 0):
+        elif(self.temp != 0):
+            size = 30
+            s = self.temp
+            pos_x = x + 5
+            pos_y = y + 5
             
         if(self.selected):
             pygame.draw.rect(win, (255,0,0), (x, y, self.width, self.height), 4)
         
-        string = font.render(str(s), 1, (125, 125, 125))
+        font = pygame.font.SysFont('Comic Sans MS', size)
+        string = font.render(str(s), 1, color)
         win.blit(string, (pos_x, pos_y))
 
 
@@ -85,28 +86,50 @@ class Grid:
     def reset(self):
         for i in range(0, self.row):
             for j in range(0, self.col):
-                self.cubes[i][j].temp = 0
-                self.cubes[i][j].val = 0
+                if(not self.cubes[i][j].is_grid):
+                    self.cubes[i][j].temp = 0
+                    self.cubes[i][j].val = 0
 
         print('RESET')
     
-    def fix_cubes(self):
+    def new_game(self):
         for i in range(0, self.row):
             for j in range(0, self.col):
-                if(self.cubes[i][j].val == 0):
-                    self.cubes[i][j].fixed = True
+                    self.cubes[i][j].temp = 0
+                    self.cubes[i][j].val = 0
+                    self.cubes[i][j].is_grid = False
+                    self.cubes[i][j].selected = False
+        self.selected = None
+
+        print('NEW')
+    
+    def fix_grid(self):
+        gr = []
+        for i in range(0, self.row):
+            temp = []
+            for j in range(0, self.col):
+                if(self.cubes[i][j].val != 0):
+                    self.cubes[i][j].is_grid = True
+                    self.cubes[i][j].color = (100, 100, 100)
+                temp.append(self.cubes[i][j].val)
+            gr.append(temp)
+        
+            print('FIX')
 
     def set_temp(self, k):
         if(self.selected):
             i = self.selected[0]
             j = self.selected[1]
-            self.cubes[i][j].temp = k
-            print(i, j, k)
+            if(not self.cubes[i][j].is_grid):
+                self.cubes[i][j].temp = k
+                self.cubes[i][j].val = 0
     
     def set_val(self, k):
         if(self.selected):
             i = self.selected[0]
             j = self.selected[1]
+            if(k == 0):
+                k = self.cubes[i][j].temp
             self.cubes[i][j].val = k
 
     def select(self, ii, jj):
@@ -116,8 +139,49 @@ class Grid:
 
         self.cubes[ii][jj].selected = True
         self.selected = (ii, jj)
+        # print(ii, jj)
+    
+    def random(self):
+        sudoku_grid = [ [0,8,0,  0,0,0,  2,0,0],
+                    [0,0,0,  0,8,4,  0,9,0],
+                    [0,0,6,  3,2,0,  0,1,0],
+                    
+                    [0,9,7,  0,0,0,  0,8,0],
+                    [8,0,0,  9,0,3,  0,0,2],
+                    [0,1,0,  0,0,0,  9,5,0],
 
+                    [0,7,0,  0,4,5,  8,0,0],
+                    [0,3,0,  7,1,0,  0,0,0],
+                    [0,0,8,  0,0,0,  0,4,0]
+                ]
+        for i in range(len(sudoku_grid)):
+            for j in range(len(sudoku_grid[0])):
+                self.cubes[i][j].val = sudoku_grid[i][j]
+        
+        print('RANDOM')
 
+    def check_solution(self):
+        grid = []
+        for i in range(0, self.row):
+            temp = []
+            for j in range(0, self.col):
+                temp.append(self.cubes[i][j].val)
+            grid.append(temp)
+        
+        result = deepcopy(grid)
+        solve(result, DIM)
+        
+        correct = True
+        for i in range(0, self.row):
+            for j in range(0, self.col):
+                if(result[i][j] != grid[i][j]):
+                    correct = False
+        
+        if(correct):
+            print("Sudoku solved!")
+        else:
+            print('Sudoku not solved. Restart it')
+                
     def get_clicked_cube(self, mouse_pos):
             space_x = self.width / self.row
             space_y = self.height / self.col
@@ -181,9 +245,9 @@ class Button:
         if(pos[0] > self.x and pos[0] < self.x + self.width):
             if pos[1] > self.y and pos[1] < self.y + self.height:
                 self.color = self.color_list[1]
-                return True
+                return (True, self.text)
 
-        return False
+        return (False, self.text)
 
 
 class Sudoku_GUI:
@@ -197,34 +261,34 @@ class Sudoku_GUI:
         self.time = 0
         self.started = False
 
-        # new_btn = Button(GREEN_BUTTON_OFF, GREEN_BUTTON_ON, 20, self.height - 105, 170, 70, f=self.grid.reset, text='New game')
-        # reset_btn = Button(YELLOW_BUTTON_OFF, YELLOW_BUTTON_ON, 220, self.height - 105, 190, 70, f=self.grid.reset, text='Restart')
-        # solve_btn = Button(RED_BUTTON_OFF, RED_BUTTON_ON, 520, self.height - 85, 185, 70, f=self.grid.reset, text='Solve game')
-        
-        new_btn = Button(GREEN_BUTTON_OFF, GREEN_BUTTON_ON, 20, self.height - 105, 170, 70, f=self.grid.reset, text='New game')
+        new_btn = Button(GREEN_BUTTON_OFF, GREEN_BUTTON_ON, 20, self.height - 105, 170, 70, f=self.grid.new_game, text='New game')
+        random_btn = Button(YELLOW_BUTTON_OFF, YELLOW_BUTTON_ON, 220, self.height - 105, 190, 70, f=self.grid.random, text='Random')
         reset_btn = Button(YELLOW_BUTTON_OFF, YELLOW_BUTTON_ON, 220, self.height - 105, 190, 70, f=self.grid.reset, text='Restart')
-        solve_btn = Button(RED_BUTTON_OFF, RED_BUTTON_ON, 520, self.height - 85, 185, 70, f=self.grid.reset, text='Solve game')
-        start_btn = Button(GREEN_BUTTON_OFF, GREEN_BUTTON_ON, 20, self.height - 105, 170, 70, f=self.grid.fix_cubes, text='Start game')
-        
-        self.buttons = [new_btn, reset_btn, solve_btn, start_btn]
+        solve_btn = Button(RED_BUTTON_OFF, RED_BUTTON_ON, 520, self.height - 85, 185, 70, f=self.grid.check_solution, text='Solve game')
+        start_btn = Button(GREEN_BUTTON_OFF, GREEN_BUTTON_ON, 20, self.height - 105, 170, 70, f=self.grid.fix_grid, text='Start game')
+        self.b = [new_btn, reset_btn, solve_btn, random_btn, start_btn]
 
         self.execute()
-        
+
     def execute(self):
         self.run = True
-        key = None
+        
         while(self.run):
             clock = pygame.time.Clock()
             clock.tick(60)
             pos = pygame.mouse.get_pos()
+            key = 0
+
+            if(self.started):
+                self.buttons = self.b[:-2]
+            else:
+                self.buttons = self.b[-2:]
 
             for event in pygame.event.get():
                 if(event.type == pygame.QUIT):
                     self.run = False
 
                 if(event.type == pygame.KEYDOWN):
-                    # if(event.key == pygame.K_0 or event.key == pygame.K_KP0):
-                    #     key = 0
                     if(event.key == pygame.K_1 or event.key == pygame.K_KP1):
                         key = 1
                     if(event.key == pygame.K_2 or event.key == pygame.K_KP2):
@@ -243,34 +307,64 @@ class Sudoku_GUI:
                         key = 8
                     if(event.key == pygame.K_9 or event.key == pygame.K_KP9):
                         key = 9
-                    # if(event.key == pygame.K_0 or event.key == pygame.K_KP0):
-                    #     key = 0
-                    # if(event.key == pygame.K_1 or event.key == pygame.K_KP1):
-                    #     key = 1
-                    # if(event.key == pygame.K_0 or event.key == pygame.K_KP0):
-                    #     key = 0
+                    
+                    if(self.grid.selected):
+                        if(event.key == pygame.K_LEFT):
+                            i = self.grid.selected[0] - 1
+                            j = self.grid.selected[1]
+                            if i < 0:
+                                i = 0
+                            self.grid.select(i, j)
+                        if(event.key == pygame.K_RIGHT):
+                            i = self.grid.selected[0] + 1
+                            j = self.grid.selected[1]
+                            if i > self.grid.row:
+                                i = self.grid.row
+                            self.grid.select(i, j)
+                        if(event.key == pygame.K_UP):
+                            i = self.grid.selected[0]
+                            j = self.grid.selected[1] - 1
+                            if j > self.grid.col:
+                                j = self.grid.col
+                            self.grid.select(i, j)
+                        if(event.key == pygame.K_DOWN):
+                            i = self.grid.selected[0]
+                            j = self.grid.selected[1] + 1
+                            if j < 0:
+                                j = 0
+                            self.grid.select(i, j)
+                        
+                        
+
+
+                    if(event.key == pygame.K_RETURN):   ################################### controllare
+                        key = 0
+
                     if(not self.started):
                         self.grid.set_val(key)
-
-                    if(event.key == pygame.K_RETURN):
-                        key = 0
-                    if(event.key == pygame.K_RETURN):
-                        self.grid.set_val(key)
-
+                    else:
+                        if(key != 0):
+                            self.grid.set_temp(key)
+                        if(event.key == pygame.K_RETURN):
+                            self.grid.set_val(key)
 
                 if(event.type == pygame.MOUSEMOTION):
                     for b in self.buttons:
-                        if(b.is_over(pos)):
+                        if(b.is_over(pos)[0]):
                             b.color = b.color_list[1]
                         else:
                             b.color = b.color_list[0]
 
                 if(event.type == pygame.MOUSEBUTTONDOWN and event.button == 1):
-                    
                     if(pos[1] > WIDTH):     # check button click
                         for b in self.buttons:
-                            if(b.is_over(pos)):
+                            if(b.is_over(pos)[0]):
                                 b.fun()
+                                if(b.is_over(pos)[1] == 'New game'):
+                                    self.started = False
+                                elif(b.is_over(pos)[1] == 'Start game'):
+                                    self.started = True
+
                     else:                   # check grid click
                         i, j = self.grid.get_clicked_cube(pos)
                         self.grid.select(i, j)
